2e6fbab8fa48642edbd2f5801b1af05aa93aaaf0
John McCormick <johnmccormick91@gmail.com>
2020-05-29 07:50:05 +0100

    Add files via upload

--- js/jm-math.js
+++ js/jm-math.js
@@ -2,3 +2,7 @@ function toRadians(degrees) {
     var radians = degrees * (Math.PI / 180)
     return radians;
 }
+
+function map_range(value, lowIn, highIn, lowOut, highOut) {
+    return lowOut + (highOut - lowOut) * (value - lowIn) / (highIn - lowIn);
+}
